84eb635006f8ed1f5f6af05b27814aa27ebae16f
Sanjay Sanjay <sanjaysanjay@Sanjays-MacBook-Air.local>
2024-11-16 17:11:06 +0530

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+import Missing from "./pages/Missing";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-3xl font-bold underline">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/video/:id' element={<Video />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
