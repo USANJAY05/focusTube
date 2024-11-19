57b16c17ecb20333ddc43b0ee177244d2692bac6
Sanjay Sanjay <sanjaysanjay@Sanjays-MacBook-Air.local>
2024-11-19 23:28:56 +0530

    hide rightsection

--- src/components/navBar/RightSection.jsx
+++ src/components/navBar/RightSection.jsx
@@ -6,8 +6,8 @@ import profile from '../../assets/icons/contact.png'
 const RightSection = () => {
   return (
     <div className='flex items-center gap-3 text-2xl'>
-        <RiVideoAddLine className='w-6 h-6 p-2 dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' />
-        <IoMdNotificationsOutline className='w-6 h-6 p-2 bg-white dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' />
+        {/* <RiVideoAddLine className='w-6 h-6 p-2 dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' /> */}
+        {/* <IoMdNotificationsOutline className='w-6 h-6 p-2 bg-white dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' /> */}
         <img src={profile} className='w-8 h-8' alt="profile-icon" />
     </div>
   )
--- src/components/navBar/Search.jsx
+++ src/components/navBar/Search.jsx
@@ -5,13 +5,14 @@ import { FaMicrophone } from "react-icons/fa";
 const Search = () => {
   return (
     <div className='flex gap-5 justify-center items-center w-1/2'>
-        <div className='flex w-3/4 pr-4 items-center rounded-3xl dark:bg-slate-800 bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700'>
+        <div className='flex w-4/5 h-10 relative'>
             <input type="text" 
                 placeholder='Search'
                 tabIndex={1}
                 aria-label='search-box'
-                className='w-full h-10 dark:bg-black outline -outline-offset-2 dark:outline-gray-700 outline-gray-300 focus:outline-blue-500 rounded-l-3xl py-1 px-5 outline-1 focus:outline' />
-            <CiSearch className='w-8 h-8 ml-3' />
+                className='w-full h-full  -mr-3 dark:bg-black outline -outline-offset-2 dark:outline-gray-700 outline-gray-300 focus:outline-blue-500 rounded-l-3xl py-1 px-5 outline-1 focus:outline' 
+              />
+            <CiSearch className='w-10 h-full ml-3 px-3 rounded-r-2xl box-content bg-gray-300 dark:bg-slate-800' />
         </div>
         <FaMicrophone className='text-1xl dark:bg-slate-800 hover:bg-gray-200 dark:hover:bg-gray-700 bg-gray-100 box-content rounded-3xl p-3' />
     </div>
