2d762c3ea0126937928348d73bfee251f0165f7b
Sanjay Sanjay <sanjay.05@icloud.com>
2024-11-27 21:08:36 +0530

    added condition for scrollbar

--- src/components/scrollBar/ScrollBar.jsx
+++ src/components/scrollBar/ScrollBar.jsx
@@ -18,6 +18,7 @@ const ScrollBar = () => {
           key: import.meta.env.VITE_API_KEY
         }
       });
+      console.log(response.data)
       return response.data;
     } catch (error) {
       console.error('Error fetching categories: ', error);
@@ -42,8 +43,9 @@ const ScrollBar = () => {
         {categories.map((category) => (
           <li 
             key={category.id}
-            className={`px-2 py-1 w-auto text-sm font-semibold rounded-lg dark:hover:bg-gray-700 hover:bg-gray-400 dark:bg-slate-800
-               bg-gray-100 dark:text-white hover:cursor-pointer scrollbar-none ${category.id == active?'dark:bg-gray-400 bg-gray-400':''}`}
+            className={`px-2 py-1 w-auto text-sm font-semibold rounded-lg dark:hover:bg-gray-700 hover:bg-gray-400 dark:bg-slate-800 
+              ${category.snippet.assignable!==true ? 'hidden':''}
+              bg-gray-100 dark:text-white hover:cursor-pointer scrollbar-none ${category.id == active?'dark:bg-gray-400 bg-gray-400':''}`}
             onClick={() => dispatch(setScrollActive(category.id))}
           >
             {category.snippet.title} {/* Display the category title */}
