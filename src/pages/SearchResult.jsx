f16c8d9b6bef3a85eacb7e7f96e0318b8e5970bb
Sanjay Sanjay <sanjay.05@icloud.com>
2024-11-30 01:04:03 +0530

    added link to search result

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ScrollBar from '../components/scrollBar/ScrollBar';
-import Thumnail from '../components/main/Thumnail';
-import { useParams } from 'react-router';
+import { useParams, Link } from 'react-router-dom';
 import DesktopThumnail from '../components/main/DesktopThumnail';
 
 const SearchResult = () => {
@@ -14,7 +12,7 @@ const SearchResult = () => {
         `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&maxResults=5&key=${import.meta.env.VITE_API_KEY}`
       );
       const data = await response.json();
-      console.log(data.items)
+      console.log(data.items);
       setItems(data.items || []);
     } catch (error) {
       console.error('Error searching videos:', error);
@@ -28,7 +26,6 @@ const SearchResult = () => {
   return (
     <div className="flex w-full h-full overflow-auto bg-black">
       <div className="flex flex-col h-full w-full overflow-auto">
-        {/* <ScrollBar /> */}
         <section className="w-full dark:text-white dark:bg-black overflow-auto flex flex-col gap-8">
           {items.map((item, idx) => {
             const isChannel = item.id.kind === 'youtube#channel';
@@ -38,23 +35,28 @@ const SearchResult = () => {
             return (
               <section
                 key={idx}
-                className="w-full flex gap-10 items-center justify-between p-5 "
+                className="w-full flex gap-10 items-center justify-between p-5"
               >
                 {isChannel && (
                   <>
-                    <div className="w-1/4 flex justify-center items-center">
+                    <Link
+                      to={`/channel/${item.id.channelId}`}
+                      className="w-1/4 flex justify-center items-center"
+                    >
                       <img
                         src={snippet.thumbnails.default.url}
                         alt={snippet.channelTitle}
                         className="w-32 h-32 rounded-full"
                       />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                      <h1 className='font-bold text-xl'>{snippet.channelTitle}</h1>
-                      <p>{snippet.description}</p>
+                    </Link>
+                    <div className="flex flex-col gap-2">
+                      <Link to={`/channel/${item.id.channelId}`}>
+                        <h1 className="font-bold text-xl">{snippet.channelTitle}</h1>
+                        <p className="text-gray-400">{snippet.description}</p>
+                      </Link>
                       <div>
-                        <button className="bg-slate-600 rounded-3xl px-4 py-2">
-                            Subscribe
+                        <button className="bg-slate-600 hover:bg-slate-500 rounded-3xl px-4 py-2">
+                          Subscribe
                         </button>
                       </div>
                     </div>
@@ -62,16 +64,16 @@ const SearchResult = () => {
                 )}
 
                 {isVideo && (
-                <div className='w-full'>
+                  <div className="w-full">
                     <DesktopThumnail
-                        channelId={snippet.channelId}
-                        channel={snippet.channelTitle}
-                        thumnail={snippet.thumbnails.medium.url}
-                        id={item.id.videoId}
-                        title={snippet.title}
-                        date={snippet.publishedAt}
+                      channelId={snippet.channelId}
+                      channel={snippet.channelTitle}
+                      thumnail={snippet.thumbnails.medium.url}
+                      id={item.id.videoId}
+                      title={snippet.title}
+                      date={snippet.publishedAt}
                     />
-                </div>
+                  </div>
                 )}
               </section>
             );
