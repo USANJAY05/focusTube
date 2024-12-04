c85fba6c277baba6ef22ff6da2569382516c054f
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-04 20:34:02 +0530

    modified player

--- src/components/common/SignupBtn.jsx
+++ src/components/common/SignupBtn.jsx
@@ -3,7 +3,7 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 
 const SignupBtn = ({text}) => {
   return (
-    <div className='px-3 py-1 text-sm rounded-3xl border hover:bg-blue-100 hover:border-white dark:text-white dark:border-slate-500 dark:hover:bg-slate-800 dark:hover:border-black text-blue-400 flex items-center gap-1'>
+    <div className='px-3 py-1 text-sm rounded-3xl border hover:cursor-pointer hover:bg-blue-100 hover:border-white dark:text-white dark:border-slate-500 dark:hover:bg-slate-800 dark:hover:border-black text-blue-400 flex items-center gap-1'>
         <IoPersonCircleOutline className='w-6 h-6' />
         <button>{text}</button>
     </div>
--- src/components/video/Player.jsx
+++ src/components/video/Player.jsx
@@ -5,7 +5,7 @@ const Player = ({ id }) => {
     <div>
       <iframe
         className="w-full sm:h-[calc(100vh-400px)] h-[37vh] xl:h-[calc(100vh-300px)] rounded-lg"
-        src={`https://www.youtube.com/embed/${id}?autoplay=1&controls=1&loop=1&playlist=${id}&modestbranding=1&rel=0&fs=1`} // Make sure fs=1 allows fullscreen
+        src={`https://www.youtube.com/embed/${id}?autoplay=1&controls=1&loop=0&rel=0&fs=1`} // Make sure fs=1 allows fullscreen
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" // Make sure fullscreen is allowed
         referrerPolicy="strict-origin-when-cross-origin"
