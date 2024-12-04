64d3b0ab72f2b3510f0e9f95434d1c6b9a6762f6
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-04 12:06:00 +0530

    added signup component

--- src/components/navBar/RightSection.jsx
+++ src/components/navBar/RightSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { RiVideoAddLine } from "react-icons/ri";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import profile from '../../assets/icons/contact.png'
-import { IoPersonCircleOutline } from "react-icons/io5";
+import SignupBtn from '../common/SignupBtn';
 
 
 const RightSection = () => {
@@ -11,10 +11,7 @@ const RightSection = () => {
         {/* <RiVideoAddLine className='w-6 h-6 p-2 dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' /> */}
         {/* <IoMdNotificationsOutline className='w-6 h-6 p-2 bg-white dark:bg-black  box-content rounded-3xl dark:hover:bg-gray-600 hover:bg-gray-200' /> */}
         {/* <img src={profile} className='w-8 h-8' alt="profile-icon" /> */}
-        <div className='px-3 py-1 text-sm rounded-3xl border hover:bg-blue-100 hover:border-white dark:text-white dark:border-slate-500 dark:hover:bg-slate-800 dark:hover:border-black text-blue-400 flex items-center gap-1'>
-          <IoPersonCircleOutline className='w-6 h-6' />
-          <button>Signup</button>
-        </div>
+        <SignupBtn />
     </div>
   )
 }
