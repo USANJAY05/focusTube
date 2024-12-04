cbcebca89a7c6f6fe8382af60864111b75fde5b2
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-04 12:00:10 +0530

    modified search bar

--- src/components/navBar/Search.jsx
+++ src/components/navBar/Search.jsx
@@ -23,7 +23,7 @@ const Search = () => {
           className="w-full h-full -mr-3 dark:bg-black border outline-none border-slate-400 dark:border-slate-700 focus:border-blue-500 rounded-l-3xl py-1 px-5"
         />
         <CiSearch
-          className="w-10 h-full ml-3 px-3 rounded-r-2xl box-content bg-gray-300 dark:bg-slate-800"
+          className="w-10 h-full ml-3 px-3 rounded-r-2xl box-content bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-700 dark:bg-slate-800"
           onClick={() => searchItem(search)}
         />
       </div>
