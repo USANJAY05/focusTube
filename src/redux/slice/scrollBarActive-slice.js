33a9853094f4b2385806e9b2d3ad8cb73ce35ef9
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-06 23:13:57 +0530

    modified

--- src/redux/slice/scrollBarActive-slice.js
+++ src/redux/slice/scrollBarActive-slice.js
@@ -8,7 +8,6 @@ const scrollBarActiveSlice = createSlice({
     reducers: {
         setScrollActive: (state, action) => {
             state.value = action.payload;
-            console.log(state.value,"hiiii")
         }
     }
 })
