92ba8c05be5bd5db1b3b3c113d063857127d4e96
Sanjay Sanjay <sanjay.05@icloud.com>
2024-12-07 10:20:30 +0530

    added dropdown

---
+++ src/components/common/DropDown.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const DropDown = ({shareLink, setToggle, id}) => {
+
+    const share = async() => {
+        setToggle(false)
+        await navigator.clipboard.writeText(shareLink)
+        alert('Link copied')
+    }
+    const embedLink =async() =>{
+        setToggle(false)
+        await navigator.clipboard.writeText(`
+      <iframe
+        className="w-full sm:h-[calc(100vh-400px)] h-[37vh] xl:h-[calc(100vh-300px)] rounded-lg"
+        src={'https://www.youtube.com/embed/${id}?autoplay=1&controls=1&loop=0&rel=0&fs=1'} // Make sure fs=1 allows fullscreen
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" // Make sure fullscreen is allowed
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>`)
+        alert('Copied code')
+    }
+  return (
+    <div>
+        
+    <div className="absolute top-5 right-5 mt-2 w-52 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md">
+      <ul className="py-2">
+        <li
+            onClick={() => share()}
+         className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+          Copy Link
+        </li>
+        <li className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+          Save to watch later
+        </li>
+        <li className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+          Save to playlist
+        </li>
+        <li 
+            onClick={() => embedLink()}
+        className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+          copy embed link
+        </li>
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+export default DropDown
--- src/components/main/Thumnail.jsx
+++ src/components/main/Thumnail.jsx
@@ -1,13 +1,15 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 import count from '../../utils/count.js'
+import DropDown from '../common/DropDown.jsx';
 
 const Thumnail = ({ channelId, channel, thumnail, title, views, date, id }) => {
 
   // Ensure 'title' is defined and a string
   const safeTitle = title || 'Untitled'; // Fallback in case 'title' is undefined or null
+  const [toggle, setToggle] = useState(false)
 
   return (
     <div className='dark:text-white w-full mt-1 hover:cursor-pointer rounded-lg dark:active:bg-gray-900 active:bg-gray-200'>
@@ -18,7 +20,7 @@ const Thumnail = ({ channelId, channel, thumnail, title, views, date, id }) => {
           className='rounded-lg w-[100%]'
         />
       </Link>
-      <div className='flex justify-between mt-2 pl-5'>
+      <div className='flex justify-between mt-2 pl-5 relative'>
           <div className='inline-block w-full'>
             {/* Safe title check */}
             <Link to={`/video/${id}`}>
@@ -35,9 +37,11 @@ const Thumnail = ({ channelId, channel, thumnail, title, views, date, id }) => {
               {count(views)} * {moment(date).fromNow()}
             </p>
           </div>
-        <div className=''>
+        <div onClick={() => setToggle(!toggle)}>
           <BsThreeDotsVertical className='dark:hover:bg-gray-600 p-2 box-content rounded-2xl' />
         </div>
+        {toggle && 
+        <DropDown id={id} shareLink={`${import.meta.env.VITE_REDIRECT_URL}/video/${id}`} setToggle={setToggle} />}
       </div>
     </div>
   );
