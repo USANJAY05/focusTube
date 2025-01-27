8ff1a874ab06aa3bd35c73e7d820eb80e6d97f51
Sanjay Sanjay <sanjay.05@icloud.com>
2025-01-27 21:57:46 +0530

    handled navigate

--- src/components/common/ProfileDropDown.jsx
+++ src/components/common/ProfileDropDown.jsx
@@ -56,7 +56,7 @@ const DropDown = ({ img, name, email, setToggle }) => {
         <li className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
           Location
         </li>
-        <li className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+        <li onClick={() => navi('/settings')}  className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
           Settings
         </li>
         <li
